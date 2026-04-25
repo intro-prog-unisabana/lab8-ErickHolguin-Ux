d20676db4a3ebc01ec8d034ace749b7d92384408
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-04-25 14:04:57 -0500

    Try 1 question 1

--- question1.py
+++ question1.py
@@ -4,3 +4,17 @@ Implementa una CLI que calcule carga por punto de soporte.
 """
 
 # TODO: Implementar según README.md
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print("Error: Invalid input! Enter numeric values only.")
+        return
+    
+    total_arg = sys.argv[1]
+    num_supports_arg = sys.argv[2]
+
+    if not total_arg.replace('.', '', 1).isdigit() or not num_supports_arg.isdigit():
+        print("Error: Invalid input! Enter numeric values only.")
+        return
+    
